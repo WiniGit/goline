fef6e78a78417b1213e47e9e05d5f9afa22a9384
thuno <thuno2698@gmail.com>
2023-06-06 13:48:58 +0700

    fix layer when create new

--- script/js/design-edit-wbase-style-events.js
+++ script/js/design-edit-wbase-style-events.js
@@ -2383,18 +2383,14 @@ function changeProperty(variantID) {
       if (wbaseParent) {
         wbaseParent.ListChildID[indexOf(selectedWbase.GID)] = newWbaseSelect.GID;
       }
-      let newWbaseLayer = createLayerTile(newWbaseSelect);
-      document.getElementById("wbaseID:" + selectedWbase.GID).parentElement.replaceWith(newWbaseLayer);
       let seletedHTML = document.getElementById(selectedWbase.GID);
       let seletedComputeStyle = window.getComputedStyle(seletedHTML);
       newWbaseSelect.value.style.position = seletedComputeStyle.position;
       newWbaseSelect.value.style.left = seletedComputeStyle.left;
       newWbaseSelect.value.style.top = seletedComputeStyle.top;
       seletedHTML.replaceWith(newWbaseSelect.value);
-      if (newWbaseSelect.Level == 1) {
-        document.getElementById("title:" + selectedWbase.GID)?.remove();
-      }
       selectedWbase.IsDeleted = true;
+      replaceAllLyerItemHTML();
     }
     selected_list = [];
     WBaseDA.changeProperty(listUpdate);
