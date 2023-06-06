68db4b3cc95bcb1d35498a9dd09c7702c3f285ec
thuno <thuno2698@gmail.com>
2023-06-06 13:55:27 +0700

    fix unchecked-bg

--- script/js/design-edit-wbase-style-events.js
+++ script/js/design-edit-wbase-style-events.js
@@ -2407,7 +2407,7 @@ function editJsonItem(jsonItem, onSubmit = true) {
     selected_list[0].JsonItem.InactiveColor = jsonItem.InactiveColor;
     if (selected_list[0].JsonItem.Content !== "true") {
       selected_list[0].value.style.backgroundColor = `#${jsonItem.InactiveColor.substring(2) + jsonItem.InactiveColor.substring(2, 0)}`;
-      selected_list[0].value.setAttribute("inactiveColor", jsonItem.InactiveColor);
+      selected_list[0].value.style.setProperty("--unchecked-bg", `#${jsonItem.InactiveColor.substring(2) + jsonItem.InactiveColor.substring(2, 0)}`);
     }
   } else if (jsonItem.Enable != undefined) {
     selected_list[0].JsonItem.Enable = jsonItem.Enable;
