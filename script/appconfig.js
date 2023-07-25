ee89b9729320be0ba1abc2ee234b10f64dedc1dd
thuno <thuno2698@gmail.com>
2023-07-25 18:02:23 +0700

    .

--- script/appconfig.js
+++ script/appconfig.js
@@ -10,10 +10,10 @@ var urlFile = "https://file.wini.vn/";
 // var urlFile = "https://demo.wini.vn/";
 var downloadUrl = "http://download.wini.vn/";
 
-// var domainApi = 'http://192.168.1.9:82/';
+var domainApi = 'http://192.168.1.9:82/';
 
-// var socketHome = "ws://192.168.1.18:6001";
-// var socketWini = "ws://192.168.1.18:4000";
+var socketHome = "ws://192.168.1.18:6001";
+var socketWini = "ws://192.168.1.18:4000";
 
 // var urlFile = "http://192.168.1.18:86/";
 
