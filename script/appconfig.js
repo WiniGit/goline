4d8db1618b9bab75e12032049e86ea8855c11dd1
thunt77 <thunt77@fpt.com>
2024-01-05 13:31:37 +0700

    socketWini

--- script/appconfig.js
+++ script/appconfig.js
@@ -3,7 +3,7 @@ var domainUrl = 'http://localhost:3000/';
 var domainApi = 'https://apip.wini.vn/';
 // 
 var socketHome = "wss://home.wini.vn";
-// var socketWini = "wss://server.wini.vn";
+var socketWini = "wss://server.wini.vn";
 var socketWiniFile = "https://server.wini.vn";
 
 var urlFile = "https://file.wini.vn/";
@@ -13,7 +13,7 @@ var downloadUrl = "http://download.wini.vn/";
 // var domainApi = 'http://192.168.1.20:82/';
 
 // var socketHome = "ws://192.168.1.20:6001";
-var socketWini = "ws://10.15.144.126:4000";
+// var socketWini = "ws://10.15.144.126:4000";
 
 // var urlFile = "http://192.168.1.18:86/";
 
